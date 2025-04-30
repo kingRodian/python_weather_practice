c189104888915492fc9b8c09e9020219af480f07
Tor Erland Hansen <king.torhansen@gmail.com>
2025-04-30 11:53:52 +0200

    Added averages option to show averages over certain timeperiods rather than per-hour.

--- TimeSeries.py
+++ TimeSeries.py
@@ -9,6 +9,13 @@ class TimeSeries:
     """
 
     TOP_REPR_FMT = 'Temperatures for {location} {date}:\n'
+    AVGS_FMT = '''Averages:
+00:00-08:00 - {}
+08:00-12:00 - {}
+12:00-18:00 - {}
+18:00-00:00 - {}
+'''
+    AVG_RANGES = [(0, 8), (8, 12), (12, 18), (18, 23)]
 
     def __init__(self, location):
         self.timepoints = dict()
@@ -34,23 +41,55 @@ class TimeSeries:
     def str_timepoints_date(self, date):
         # Return timepoints for a certain date as a printable string
         output = self.TOP_REPR_FMT.format(location=self.location, date=date.isoformat())
-        for point in self.timepoints[date]:
+        tot_avg = 0
+        points = self.timepoints[date]
+        for point in points:
+            tot_avg = tot_avg + point.temperature()
             output += str(point)
             output += '\n'
+        tot_avg = tot_avg/ len(points)
+        output += 'Average: {:.2f}.\n'.format(tot_avg)
         return output
 
-    def print_date(self, date):
-        if date in self.timepoints:
-            print(self.str_timepoints_date(date))
-        else:
-            print("No data for date: {}".format(date.isoformat()))
+    def str_avgs(self, date):
+        output = self.TOP_REPR_FMT.format(location=self.location, date=date.isoformat())
+        avgs, tot_avg  = self.calculate_avg(date)
+        outputs = ['{:.2f} deg celsius.'.format(avg) if avg else "No data" for avg in avgs]
+        output += self.AVGS_FMT.format(*outputs)
+        output += 'Total: {:.2f} deg celsius.\n'.format(tot_avg)
+        return output
 
-    def print_days(self, days):
+    def calculate_avg(self, date):
+        # Calculate averages for times 0-8, 8-12, 12-6 and 6-0
+        # and return them as a list of avgs or None values if empty, along with the total avg
+        points = self.timepoints[date]
+        vals = [[] for _ in range(len(self.AVG_RANGES))]
+        avgs = [None for _ in range(len(self.AVG_RANGES))]
+        tot_avg = 0
+        for point in points:
+            hour = point.time.hour
+            for i in range(len(self.AVG_RANGES)):
+                if hour >= self.AVG_RANGES[i][0] and hour <= self.AVG_RANGES[i][1]:
+                    tot_avg += point.temperature()
+                    vals[i].append(point.temperature())
+        for i, val in enumerate(vals):
+            # Dont divide by 0
+            if len(val) > 0:
+                avgs[i] = sum(val) / len(val)
+        return avgs, tot_avg / len(points)
+
+    def print_days(self, days, averages=False):
         today = datetime.date.today()
         curdate = today
         for day in range(days):
             curdate = today + datetime.timedelta(days=day)
-            self.print_date(curdate)
+            if curdate in self.timepoints:
+                if averages:
+                    print(self.str_avgs(curdate))
+                else:
+                    print(self.str_timepoints_date(curdate))
+            else:
+                print("No data for date: {}".format(curdate.isoformat()))
 
     def __repr__(self):
         # Print out the entire timeseries
